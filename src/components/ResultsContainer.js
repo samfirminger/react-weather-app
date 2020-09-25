d0c562146bf1effaf03d2c86597d7dfadef5fce8
Sam Firminger <Sam.Firminger@dyson.com>
2020-09-25 16:48:16 +0100

    Added mobile layout

--- src/components/ResultsContainer.js
+++ src/components/ResultsContainer.js
@@ -2,16 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 import HourlyBreakDown from "./HourlyBreakdown";
 import FiveDayForecast from "./FiveDayForecast";
+import {device} from './device';
 
 const ResultsWrapper = styled.div`
-   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 0.5fr 1fr;
-  gap: 1px 1px;
-  grid-template-areas:
-    "Location Location Location Location"
-    "Current Current Hourly Hourly"
-    "Five-Day Five-Day Five-Day Five-Day";
+
+    @media ${device.mobileS} { 
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-template-rows: 0.5fr 1fr 1fr 1fr;
+        gap: 1px 1px;
+        grid-template-areas:
+        "Location Location Location Location"
+        "Current Current Current Current"
+        "Hourly Hourly Hourly Hourly"
+        "Five-Day Five-Day Five-Day Five-Day";
+    }
+    
+    @media ${device.tablet} {
+        display: grid;
+        grid-template-rows: auto 0.5fr 1fr;
+        gap: 1px 1px;
+        grid-template-areas:
+        "Location Location Location Location"
+        "Current Current Hourly Hourly"
+        "Five-Day Five-Day Five-Day Five-Day";
+    }
+
 `;
 
 const Location = styled.h2`
@@ -41,13 +57,12 @@ const ResultsContainer = ({weatherInfo, todayForecast, groupedForecast}) => {
     const futureForecast = groupedForecast;
 
 
-
     return (
         <ResultsWrapper>
             <Location className="Location">{city}, {country}</Location>
             <CurrentWeather className="Current-Weather">
-                <img alt={"current weather"} src={"http://openweathermap.org/img/wn/" +  icon + "@2x.png"}/>
-                <p>{temp}&#8451;</p>
+                <img alt={"current weather"} src={"http://openweathermap.org/img/wn/" + icon + "@2x.png"}/>
+                <h1>{temp}&#8451;</h1>
             </CurrentWeather>
             <HourlyBreakdownContainer className="Hourly">
                 <HourlyBreakDown forecast={forecast}/>
